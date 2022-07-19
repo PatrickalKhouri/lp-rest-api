1f57fcc7c5330b7140f81ec7ad5c7a7c34302601
Patrick Khouri <patrick.darya@gmail.com>
2022-07-19 18:53:24 -0300

    Did the unit tests of the UserAddress Model

---
+++ tests/unit/models/userAddress.model.test.js
@@ -0,0 +1,45 @@
+const faker = require('faker');
+const { UserAddress } = require('../../../src/models');
+
+describe('User Address model', () => {
+  describe('User Address validation', () => {
+    let newUserAddress;
+    beforeEach(() => {
+      newUserAddress = {
+        userId: faker.datatype.uuid(),
+        streetName: faker.address.street(),
+        streetNumber: faker.address.buildingNumber(),
+        postalCode: faker.address.zipCode(),
+        city: faker.address.city(),
+        state: 'Rio de Janeiro',
+        country: faker.address.country(),
+        createdAt: faker.datatype.datetime(),
+        modifiedAt: faker.datatype.datetime(),
+      };
+    });
+
+    test('should correctly validate a valid user address', async () => {
+      await expect(new UserAddress(newUserAddress).validate()).resolves.toBeUndefined();
+    });
+
+    test('should throw a validation error if country is not on list', async () => {
+      newUserAddress.country = 'non existing country';
+      await expect(new UserAddress(newUserAddress).validate()).rejects.toThrow();
+    });
+
+    test('should throw a validation error if state is not on list', async () => {
+      newUserAddress.number = 40;
+      await expect(new UserAddress(newUserAddress).validate()).rejects.toThrow();
+    });
+
+    test('should throw a validation error if the address number is an integer', async () => {
+      newUserAddress.state = 'non existing state';
+      await expect(new UserAddress(newUserAddress).validate()).rejects.toThrow();
+    });
+
+    test('should throw a validation error if the zip code number is invalid in Brazil', async () => {
+      newUserAddress.postalCode = 'not a valid zipCopde';
+      await expect(new UserAddress(newUserAddress).validate()).rejects.toThrow();
+    });
+  });
+});
