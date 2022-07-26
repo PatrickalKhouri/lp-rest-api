cacfa7d7b6f63b6a709975c7f7b917bf1a63f0f2
Patrick Khouri <patrick.darya@gmail.com>
2022-07-26 15:35:37 -0300

    Altered the duration

--- src/models/record.model.js
+++ src/models/record.model.js
@@ -26,9 +26,13 @@ const recordSchema = mongoose.Schema({
         enum: countriesLong,        
     },
     duration: {
-        type: Date,
+        type: String,
         required: true,
-        min: 0
+        validate(value) {
+            if (!value.match(/^(([0-9]?[0-9][0-9]|[0-9]):([0-5][0-9]))$/)) {
+              throw new Error('Duração está com o formato inválido');
+            }
+        },
     },
     language: {
         type: String,
